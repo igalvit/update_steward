c18c4b6f3ed536a489427873eafc7d9ce8d20393
igalvit <igalvit@gmail.com>
2022-01-28 18:25:23 +0000

    Added testing to SystemRestarter class

--- tests/test_system_restarter.py
+++ tests/test_system_restarter.py
@@ -1,6 +1,16 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from system_restarter.system_restarter import SystemRestarter
+from subprocess import CompletedProcess
+
 
 class TestSystemRestarter(unittest.TestCase):
-    pass
+
+    @patch('system_restarter.system_restarter.CommandRunner.exec_command')
+    def test_reboot_system(self, mock_command_runner : MagicMock) -> None:
+        mock_exec_command_result : CompletedProcess['bytes'] = \
+                CompletedProcess(args='shutdown -r',returncode=0,stderr=b'',stdout=b'Command executed')
+        mock_command_runner.return_value = mock_exec_command_result
+        sys_restart = SystemRestarter()
+        sys_restart.reboot_system()
+        mock_command_runner.assert_called()
